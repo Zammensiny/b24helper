32e77619941b61dbc34babe0449c8e0348fa6ca0
Kiataro <neverspaik@gmail.com>
2025-03-30 22:50:55 +0300

    https fix

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,26 @@ set -e
 
 echo 'Deploying...'
 
-cd /path/to/your/project
-
 git pull origin main
 
 php8.2 artisan down
 
-php8.2 composer install --no-dev --optimize-autoloader
+php8.2 composer i --no-dev --optimize-autoloader
 
 php8.2 artisan migrate --force
 
 php8.2 artisan config:cache
+
 php8.2 artisan event:cache
+
 php8.2 artisan route:cache
+
 php8.2 artisan view:cache
 
-npm install --production
+npm install
+
 npm run build
 
 php8.2 artisan up
 
-echo 'Deploy done!'
+echo 'Done!'
