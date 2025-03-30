3be4e436f2a7a5408285d2a94d5bc7662aefab11
Kiataro <neverspaik@gmail.com>
2025-03-30 21:07:22 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo 'Deploying...'
+
+git pull origin main
+
+php8.2 artisan down
+
+php8.2 composer i --no-dev --optimize-autoloader
+
+php8.2 artisan migrate --force
+
+php8.2 artisan config:cache
+
+php8.2 artisan event:cache
+
+php8.2 artisan route:cache
+
+php8.2 artisan view:cache
+
+php8.2 artisan up
+
+echo 'Done!'
+
+
+
+
+
+
